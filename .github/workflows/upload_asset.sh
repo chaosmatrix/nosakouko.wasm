e7ee03266b0624fc23c34ad6a00ad45ec7389c1e
chaos matrix <37551759+chaosmatrix@users.noreply.github.com>
2023-09-29 19:37:46 +0800

    Update upload_asset.sh

--- .github/workflows/upload_asset.sh
+++ .github/workflows/upload_asset.sh
@@ -32,6 +32,7 @@ echo "Git tag: $tag"
 echo "Checking for existing release..."
 upload_url=$(\
     curl \
+        --http1.1 \
         -H "Authorization: Bearer $bearer" \
         "https://api.github.com/repos/$repo/releases" \
         2> /dev/null \
@@ -50,6 +51,7 @@ if [ -z "$upload_url" ]; then
     # Create new release.
     response=$(
         curl -f \
+            --http1.1 \
             -X POST \
             -H "Authorization: Bearer $bearer" \
             -d "{\"tag_name\":\"$tag\",\"draft\":true}" \
@@ -81,6 +83,7 @@ fi
 file_name=${file_path##*/}
 echo "Uploading asset $file_name to $upload_url..."
 curl -f \
+    --http1.1 \
     -X POST \
     -H "Authorization: Bearer $bearer" \
     -H "Content-Type: application/octet-stream" \
